027129d631864b4c751679a7e5fffe8eadf37521
Rocco Panacci <rocco@clausehound.com>
2017-09-27 18:35:29 -0400

    first commit

---
+++ js/main.js
@@ -0,0 +1,23 @@
+// Generate a random number between 0.000..1 and 5.9999
+// Round the number up to closest int/whole, 1 to 6
+// Do this twice, to create two random dice
+
+// If the dice roll totals 2, print "snake eyes"
+// If the dice roll totals 3, print "ace deuce"
+// If the dice roll totals 4, print "easy/hard four"
+// If the dice roll totals 5, print "fever five"
+// If the dice roll totals 6, print "easy/hard six"
+// If the dice roll totals 7, print "natural"
+// If the dice roll totals 8, print "easy/hard eight"
+// If the dice roll totals 9, print "nina nine"
+// If the dice roll totals 10, print "easy/hard ten"
+// If the dice roll totals 11, print "yo-leven"
+// If the dice roll totals 12, print "boxcar"
+
+// For all easy/hard outputs above: if the dice numbers are identical,
+// print it as "hard", else print it as "easy"
+
+// For example:
+// If a user rolls 3 and 3, the output would be "hard six"
+// If a user rolls 4 and 2, the output would be "easy six"
+// If a user rolls 5 and 1, the output would be "easy six"
